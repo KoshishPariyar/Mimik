236dd7829ed05c0f19fc998480e5dcf9473d73fd
Koshish Pariyar <koshishpariyar0@gmail.com>
2025-03-18 21:54:19 +0545

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,43 @@
+ <!-- footer -->
+ <footer class="section-p1">
+    <div class="col">
+        <img class="logo"src="assets/images/logo4.png" alt="">
+            <h4>Contact</h4>
+        <div class="address-location">
+        <p><bold>Address:</bold>Khairahani-08, Magani Chowk</p>
+        <p ><bold>Phone:</bold>+977 982-7252818</p>
+        </div>
+        <div class="follow">
+          <h4>Follow Us</h4>
+            <div class="icon">
+                <i class="fab fa-facebook"onclick="window.location.href='https://www.facebook.com/mimikwear?mibextid=ZbWKwL'"></i>
+                <i class="fab fa-instagram"onclick="window.location.href='https://www.instagram.com/mimikwear?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='"></i>
+                <i class="fab fa-tiktok"onclick="window.location.href='https://www.tiktok.com/@mimikwear?_t=8sLlLGqIdbE&_r=1'"></i>
+                <i class="fab fa-threads"onclick="window.location.href='https://www.threads.net/@mimikwear'"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <h4>About</h4>
+        <a href="about.php">About Us</a>
+        
+        <a href="contact.php">Contact Us</a>
+    </div>
+    <div class="col">
+        <h4>My Account</h4>
+        <a href="login.php">Sign In</a>
+        <a href="cart.php">View Cart</a>
+       
+    </div>
+    <div class="install">
+        <h4 style="text-align: center;">Secured Payment Gateways</h4>
+        <div class="payment">
+        <img src="assets/images/esewa.png" alt="" >
+        <img src="assets/images/khalti.png" alt="" >
+        <img src="assets/images/fonepay.png" alt="">
+    </div>
+</div>
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
